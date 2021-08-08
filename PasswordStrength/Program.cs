b5e0bc417ab826d959db0af77cae1c2627a9d920
Kevin Ketterer <kevin_ketterer@hotmail.com>
2021-08-08 11:55:14 -0500

    Completed

--- PasswordStrength/Program.cs
+++ PasswordStrength/Program.cs
@@ -4,15 +4,61 @@ namespace PasswordStrength
 {
     class Program
     {
-        private int minLength = 12;
-        private string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private string lowercase = "abcdefghijklmnopqrstuvwxyz";
-        private string digits = "0123456789";
-        private string specialChars = "~!@#$%^&*()_+|";
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var minLength = 12;
+            var score = 0;
+            var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var lowercase = "abcdefghijklmnopqrstuvwxyz";
+            var digits = "0123456789";
+            var specialChars = "~!@#$%^&*()_+|";
+
+            Console.Write("Enter your password: ");
+            var password = Console.ReadLine();
+
+            if (password != null && password.Length >= minLength)
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            }
+
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            }
+
+            switch (score)
+            {
+                case 1: 
+                    Console.Write("Your password is weak");
+                    break;
+                case 2:
+                    Console.Write("Your password is medium");
+                    break;
+                case 3:
+                    Console.Write("Your password is strong");
+                    break;
+                case 4:
+                case 5:
+                    Console.Write("Your password is extremely strong");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
