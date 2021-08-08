d91a174909d4098bca6862dbca9d0733d10aa71d
Kevin Ketterer <kketterer@dart.org>
2021-08-08 11:15:08 -0500

    Add project files.

---
+++ PasswordStrength/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PasswordStrength
+{
+    class Program
+    {
+        private int minLength = 12;
+        private string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        private string digits = "0123456789";
+        private string specialChars = "~!@#$%^&*()_+|";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
